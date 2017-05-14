651c516a0ba083abe8680ced124d98485d1b9775
Marcin-PC <martinex9411@gmail.com>
2017-05-14 19:47:30 +0200

    Added first room and primitive phone check time function

--- Assets/Scripts/TextController.cs
+++ Assets/Scripts/TextController.cs
@@ -8,97 +8,126 @@ public class TextController : MonoBehaviour {
 
     public Text text;
     private enum States {
-        cell, mirror, sheets_0, lock_0, cell_mirror, sheets_1, lock_1, 
-        corridor_0
+        bedroom_0_0, window_0, bedroom_0_1, upperCorridor_0, stairs_0
     };
     private States myState;
 
-	// Use this for initialization
-	void Start () {
-        myState = States.cell;
+    private int hours = 2;
+    private int minutes = 37;
+    private float seconds = 0.0f;
+
+    //Awake(){}
+
+    public Image phoneI; 
+    private bool phone;
+    public Text phoneClock;
+
+    private bool doorKey;
+    private bool closetKey;
+    private bool pistol;
+    private bool butterKnife;
+
+    // Use this for initialization
+    void Start () {
+        phoneI.enabled = false;
+        phoneClock.enabled = false;
+
+        myState = States.bedroom_0_0;
 	}
 	
+    void Clock()
+    {
+        seconds += Time.deltaTime;
+
+        if (seconds >= 60)
+        {
+            minutes++;
+            seconds = 0.0f;
+        }
+
+        if (minutes >= 60)
+        {
+            hours++;
+            minutes = 0;
+        }
+
+        if (hours >= 24)
+        {
+            hours = 0;
+        }
+    }
+
 	// Update is called once per frame
 	void Update () {
-        print(myState);
+        Clock();
 
-        if      (myState == States.cell)        { cell(); }
-        else if (myState == States.sheets_0)    { sheets_0(); }
-        else if (myState == States.sheets_1)    { sheets_1(); }
-        else if (myState == States.lock_0)      { lock_0(); }
-        else if (myState == States.lock_1)      { lock_1(); }
-        else if (myState == States.mirror)      { mirror(); }
-        else if (myState == States.cell_mirror) { cell_mirror(); }
-        else if (myState == States.corridor_0)  { corridor_0(); }
+        if      (myState == States.bedroom_0_0)     { bedroom_0_0(); }
+        else if (myState == States.window_0)        { window_0(); }
+        else if (myState == States.bedroom_0_1)     { bedroom_0_1(); }
     }
 
-    #region State handler methods
-    void cell() {
-        text.text = "Znajdujesz się w celi więziennej, z której chcesz uciec. " +
-                    "Na łóżku leży brudna pościel, na ścianie wisi lustro, a drzwi są zamknięte od zewnątrz.\n\n" +
-                    "Wciśnij P aby spojrzeć na Pościel, L na Lustro, Z na Zamek w drzwiach";
+    #region Equipment functions
 
-        if (Input.GetKeyDown(KeyCode.P))            { myState = States.sheets_0; }
-        else if (Input.GetKeyDown(KeyCode.L))       { myState = States.mirror; }
-        else if (Input.GetKeyDown(KeyCode.Z))       { myState = States.lock_0; }
-    }
-
-    void mirror() {
-        text.text = "Stare, brudne lustro wisi na ścianie.\n\n" +
-                    "Wciśnij Z żeby Zabrać lustro lub TAB żeby powrócić do widoku celi.";
-
-        if (Input.GetKeyDown(KeyCode.Z))            { myState = States.cell_mirror; }
-        else if (Input.GetKeyDown(KeyCode.Tab))     { myState = States.cell; }
-    }
-
-    void cell_mirror() {
-        text.text = "Nadal jesteś w celi i wciąż chcesz stąd zwiać. Na łóżku znajdują się brudne prześcieradła, " +
-                    "na ścianie hak, na którym wisiało lustro oraz drzwi na hasło, zamknięte na amen.\n\n" +
-                    "Wciśnij P aby spojrzeć na Pościel, Z na Zamek";
-
-        if (Input.GetKeyDown(KeyCode.P))            { myState = States.sheets_1; }
-        else if (Input.GetKeyDown(KeyCode.Z))       { myState = States.lock_1; }
-    }
-
-    void sheets_0() {
-        text.text = "Nie możesz uwierzyć, że na tym śpisz. " +
-                    "Na pewno już czas, żeby ją wymienić. Zalety bycia w więzieniu...\n\n" +
-                    "Wciśnij TAB, żeby powrócić do widoku celi.";
-
-        if (Input.GetKeyDown(KeyCode.Tab))          { myState = States.cell; }
-    }
-
-    void sheets_1() {
-        text.text = "Trzymanie lustra w ręku wcale nie upiększa tego prześcieradła.\n\n" +
-                    "Wciśnij TAB, żeby powrócić do widoku celi.";
-
-        if (Input.GetKeyDown(KeyCode.Tab))          { myState = States.cell_mirror; }
-    }
-
-    void lock_0() {
-        text.text = "Zamek z kodem. Nie wiesz jednak, jaki jest do niego szyfr. " +
-                    "Marzysz o tym, żeby móc dostrzec brud na przyciskach. Może to by pomogło.\n\n" +
-                    "Wciśnij TAB, żeby powrócić do widoku celi.";
-
-        if (Input.GetKeyDown(KeyCode.Tab))          { myState = States.cell; }
-    }
-
-    void lock_1() {
-        text.text = "Ostrożnie wsuwasz lustro przez więzienne kraty i obracasz tak, żeby " +
-                    "zobaczyć zamek. Widzisz, na których klawiszach farba jest bardziej zużyta. " +
-                    "Próbujesz nacisnąć kombinację i nagle słyszysz klik.\n\n" +
-                    "Wciśnij O aby Otworzyć drzwi lub TAB aby powrócić do swojej celi.";
-
-        if (Input.GetKeyDown(KeyCode.O))            { myState = States.corridor_0; }
-        else if (Input.GetKeyDown(KeyCode.Tab))     { myState = States.cell_mirror; }
-    }
-
-    void corridor_0()
+    #region Phone
+    public void Phone_use()
     {
-        text.text = "Wyszedłeś na korytarz.\n\n" +
-                    "Wciśnij R, żeby zagrać ponownie.";
-
-        if (Input.GetKeyDown(KeyCode.R))  { myState = States.cell; }
+        StartCoroutine(PhoneClock());
     }
+
+    IEnumerator PhoneClock()
+    {
+        phoneClock.enabled = true;
+
+        for (int i = 0; i<3; i++)
+        {
+            phoneClock.text = hours + ":" + minutes;
+            yield return new WaitForSeconds(0.75F);
+            phoneClock.text = hours + " " + minutes;
+            yield return new WaitForSeconds(0.75F);
+        }
+        phoneClock.enabled = false;
+    }
+    #endregion
+    
+    #endregion
+
+
+    #region Rooms functions
+
+    void bedroom_0_0()
+    {
+        text.text = "Budzisz się w nocy. Spoglądasz na zegar. Jest " + hours + ":" + minutes + ".\n" +
+                    "Nie możesz spać. Masz przeczucie, jakby coś tej nocy miało się wydarzyć.\n\n" +
+                    "-> spójrz przez [O]kno \n-> wyjdź na [K]orytarz";
+
+        if      (Input.GetKeyDown(KeyCode.O)) { myState = States.window_0; }
+        else if (Input.GetKeyDown(KeyCode.K)) { myState = States.upperCorridor_0; }
+    }
+
+    void window_0()
+    {
+        text.text = "Spoglądasz przez okno. Dzisiejszej nocy jest wyjątkowo ciemno. Blask księżyca niewidoczny, schowany za pobliskim lasem. " +
+                    "Silny wiatr zakrzywia korony drzew swoim oddechem.\n" +
+                    "Gdzieś na ulicy w oddali widać jarzącą się mocnym, żółtym światłem latarnię.\n\n" +
+                    "-> [O]dsuń się od okna";
+
+        if      (Input.GetKeyDown(KeyCode.O)) { myState = States.bedroom_0_1; }
+    }
+
+    void bedroom_0_1()
+    {
+        text.text = "Na łóżku leży rozgrzebana pościel i poduszka z odgniecionym śladem Twojej głowy.\n" +
+                    "";
+
+        if (phone == false)
+        {
+            text.text += "\n-> weź [T]elefon z szafki nocnej.";
+        }
+
+        if      (Input.GetKeyDown(KeyCode.T) && !phone) { myState = States.bedroom_0_1; phone = true; phoneI.enabled = true; }
+        else if (Input.GetKeyDown(KeyCode.K))           { myState = States.upperCorridor_0; }
+    }
+
+ 
     #endregion
 }
