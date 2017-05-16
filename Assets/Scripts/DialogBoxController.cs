7ed683537588b75b14c7f00206e2be4848b9c3dc
Marcin-PC <martinex9411@gmail.com>
2017-05-16 21:12:34 +0200

    Added DialogBox script to communicate with a player.

---
+++ Assets/Scripts/DialogBoxController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DialogBoxController : MonoBehaviour {
+
+    private Rect windowRect;
+    private bool show;              // Only show dialog if true.
+    private int dialogButtons;      // number of buttons in a dialog
+    private string dialogText;      // text to show in a dialog
+
+    void OnGUI()
+    {
+        if (show)
+        {
+            windowRect = new Rect((Screen.width - 300) / 2, (Screen.height - (75 + 25 * dialogButtons)) / 2, 300, (50 + 25 * dialogButtons));   // 300x(50 + 25 * number of buttons requested) px window will apear in the center of the screen.
+            windowRect = GUI.Window(0, windowRect, WindowDialog, "~~~ Komunikat ~~~");
+        }
+    }
+    
+
+    /// <summary> This is the actual window. </summary>
+    void WindowDialog(int windowID)
+    {
+        GUI.Label(new Rect(5, 25, windowRect.width, 20), dialogText);
+
+        for (int i = 0; i < dialogButtons; i++)
+        {
+            float newY = 25 * i;    // variable to put buttons lower if more than 1 button requested
+
+            if (GUI.Button(new Rect(5, 50 + newY, windowRect.width - 10, 20), "Ok"))
+            {
+                show = false;
+            }
+        }
+    }
+
+
+    /// <summary> Method to open the dialog from outside of the script. </summary>
+    /// <param name="text"> Text to show in a dialog. </param>
+    /// <param name="buttonsAmount"> How many buttons do you need (default 1). </param>
+    public void Open(string text, int buttonsAmount=1)
+    {
+        dialogText = text;
+        dialogButtons = buttonsAmount;
+
+        show = true;
+    }
+}
--- Assets/Scripts/TextController.cs
+++ Assets/Scripts/TextController.cs
@@ -12,17 +12,22 @@ public class TextController : MonoBehaviour {
         hall_0, bathroom_0
     };
     private States myState;
-    private States previousState;
+    private States previousState;       // not used yet
+    
+    private string currentRoom;         // not used yet         // to check if enemy is in the same room as player
+
+    public DialogBoxController dialog;
 
     private int hours = 2;
     private int minutes = 37;
     private float seconds = 0.0f;
 
     private bool enemy;
-    private bool enemySeen;
+    private bool enemySeen;             // not used yet
 
     //Awake(){}
 
+    #region Equipment
     public Image phoneI; 
     private bool phone;
     public Text phoneClock;
@@ -31,16 +36,13 @@ public class TextController : MonoBehaviour {
     private bool closetKey;
     private bool pistol;
     private bool butterKnife;
-
+    #endregion
 
     #region Clothes
     private bool shoes;
     private bool szlafrok;
     #endregion
 
-
-
-    // Use this for initialization
     void Start () {
         phoneI.enabled = false;
         phoneClock.enabled = false;
@@ -66,17 +68,16 @@ public class TextController : MonoBehaviour {
             hours = 0;
         }
     }
-
-	// Update is called once per frame
+    
 	void Update () {
         Clock();
 
-        if      (myState == States.bedroom_0_0)     { bedroom_0_0(); }
-        else if (myState == States.window_0)        { window_0(); }
-        else if (myState == States.bedroom_0_1)     { bedroom_0_1(); }
-        else if (myState == States.upperCorridor_0) { upperCorridor_0(); }
-        else if (myState == States.hall_0)          { hall_0(); }
-        else if (myState == States.bathroom_0)      { bathroom_0(); }
+        if      (myState == States.bedroom_0_0)     { bedroom_0_0(); currentRoom = "Bedroom"; }
+        else if (myState == States.window_0)        { window_0(); currentRoom = "Bedroom"; }
+        else if (myState == States.bedroom_0_1)     { bedroom_0_1(); currentRoom = "Bedroom"; }
+        else if (myState == States.upperCorridor_0) { upperCorridor_0(); currentRoom = "UpperCorridor"; }
+        else if (myState == States.bathroom_0)      { bathroom_0(); currentRoom = "Bathroom"; }
+        else if (myState == States.hall_0)          { hall_0(); currentRoom = "Hall"; }
     }
 
     #region Equipment functionality
@@ -110,9 +111,9 @@ public class TextController : MonoBehaviour {
     void bedroom_0_0()
     {
         text.text = "Budzisz się w nocy. Spoglądasz na budzik, leżący na stoliku nocnym. Jest " + hours + ":" + minutes + ".\n" +
-                    "Nie możesz spać. Masz przeczucie, jakby coś tej nocy miało się wydarzyć.\n\n" +
-                    "-> spójrz przez [O]kno\n" +
-                    "-> wyjdź na [K]orytarz";
+                    "Nie możesz spać. Masz przeczucie, jakby coś tej nocy miało się wydarzyć." +
+                    "\n-> spójrz przez [O]kno" +
+                    "\n-> wyjdź na [K]orytarz";
 
         if      (Input.GetKeyDown(KeyCode.O)) { myState = States.window_0; }
         else if (Input.GetKeyDown(KeyCode.K)) { myState = States.upperCorridor_0; }
@@ -122,8 +123,8 @@ public class TextController : MonoBehaviour {
     {
         text.text = "W półmroku podchodzisz do okna. Spoglądasz przez nie. Dzisiejszej nocy jest wyjątkowo ciemno. Blask księżyca niewidoczny, " +
                     "schowany za pobliskim lasem. Silny wiatr zakrzywia korony drzew swoim oddechem.\n" +
-                    "Gdzieś na ulicy w oddali widać jarzącą się mocnym, żółtym światłem latarnię.\n\n" +
-                    "-> [O]dsuń się od okna";
+                    "Gdzieś na ulicy w oddali widać jarzącą się mocnym, żółtym światłem latarnię." +
+                    "\n-> [O]dsuń się od okna";
 
         if      (Input.GetKeyDown(KeyCode.O)) { myState = States.bedroom_0_1; }
     }
@@ -131,28 +132,28 @@ public class TextController : MonoBehaviour {
     void bedroom_0_1()
     {
         text.text = "Na łóżku leży rozgrzebana pościel i poduszka z odgniecionym śladem Twojej głowy.\n" +
-                    "W pokoju panuje chłód.";
+                    "W pokoju panuje chłód. ";
 
-        if (shoes == false) {
-            text.text += " Panele są zimne, marzną Ci stopy.\n";
+        if (!shoes) {
+            text.text += "Panele są zimne, marzną Ci stopy.";
             text.text += "\n-> załóż [B]uty.";
         }
 
-        if (phone == false) {
-            if (shoes) { text.text += "\n"; }
-            text.text += "\n-> weź [T]elefon z szafki nocnej.";
+        if (!phone && shoes) {
+            text.text += "Kiedy schylasz się po buty, zauważasz telefon." +
+                         "\n-> weź [T]elefon z szafki nocnej.";
         }
 
-        if (szlafrok == false) {
+        if (!szlafrok) {
             text.text += "\n-> Otwórz szafę i załóż [S]zlafrok.";
         }
         
         text.text += "\n-> wyjdź na [K]orytarz.";
 
         if      (Input.GetKeyDown(KeyCode.T) && !phone)     { phone = true; phoneI.enabled = true; myState = States.bedroom_0_1; }
-        else if (Input.GetKeyDown(KeyCode.K))               { myState = States.upperCorridor_0; previousState = States.bedroom_0_1; }
+        else if (Input.GetKeyDown(KeyCode.K))               { myState = States.upperCorridor_0; previousState = States.bedroom_0_1; StartCoroutine(EnemyCountdown()); }
         else if (Input.GetKeyDown(KeyCode.B) && !shoes)     { shoes = true; myState = States.bedroom_0_1; }
-        else if (Input.GetKeyDown(KeyCode.S) && !szlafrok)  { szlafrok = true; myState = States.bedroom_0_1; }
+        else if (Input.GetKeyDown(KeyCode.S) && !szlafrok)  { szlafrok = true; myState = States.bedroom_0_1; dialog.Open("Otwierasz szafę i ubierasz szlafrok."); }
     }
 
     void upperCorridor_0()
@@ -169,9 +170,7 @@ public class TextController : MonoBehaviour {
                      "\n-> Idź do [S]ypialni" +
                      "\n-> Idz do [Ł]azienki";
 
-        StartCoroutine(EnemyCountdown());
-
-        if      (Input.GetKeyDown(KeyCode.W)) { text.text += "\n\nŚwiatło nie działa. Pewnie korki wysiadły."; }
+        if      (Input.GetKeyDown(KeyCode.W)) { dialog.Open("Światło nie działa. Pewnie korki wysiadły."); }
         else if (Input.GetKeyDown(KeyCode.Z)) { myState = States.hall_0; }
         else if (Input.GetKeyDown(KeyCode.S) && enemySeen) { myState = States.bedroom_0_1; }
         else if (Input.GetKeyDown(KeyCode.L)) { myState = States.bathroom_0; }
@@ -197,7 +196,8 @@ public class TextController : MonoBehaviour {
 
     IEnumerator EnemyCountdown()
     {
-        yield return new WaitForSeconds(5);
-        text.text += "\n\nSŁYSZYSZ DZIWNY HAŁAS";
+        yield return new WaitForSeconds(60);
+        dialog.Open("Słyszysz dziwny dźwięk...");
+        yield break;
     }
 }
